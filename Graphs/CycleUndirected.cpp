6cc6de160ccc1e6ebc89fc5bb576ccad81821ed0
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-07-15 22:24:29 +0530

    Added method for DFS

--- Graphs/CycleUndirected.cpp
+++ Graphs/CycleUndirected.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool isCycle(int V, vector<vector<int>> &edges){
+bool isCycleUsingBFS(int V, vector<vector<int>> &edges){
     vector<vector<int>> adj(V); // Adjacency list representation of the graph
     vector<bool> visited(V); // To keep track of visited nodes
     for (int i = 0; i < edges.size(); i++){ // creating the adjacency list
@@ -33,6 +33,35 @@ bool isCycle(int V, vector<vector<int>> &edges){
     }
     return false;
 }
+bool dfs(int node,int parent,vector<vector<int>>& adj, vector<bool> &visited){
+    visited[node]=true; // mark the current node as visited
+    for(auto& n:adj[node]){ // iterate through its neighbours
+        if(!visited[n]){ // if any neighbour is not visited traverse it
+            if(dfs(n,node,adj,visited)) return true; // if any neighbour has cycle return true;
+        } else if (parent != n){ // neighbour is already visited and it is not the parent 
+            return true; // loop detected
+        }
+    }
+    return false; //if loop not detected till now, which means graph doesn't have a loop
+}
+bool isCycleUsingDFS(int V, vector<vector<int>> &edges){
+    vector<vector<int>> adj(V); // Adjacency list representation of the graph
+    vector<bool> visited(V); // To keep track of visited nodes
+    for (int i = 0; i < edges.size(); i++){ // creating the adjacency list
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    bool ans=true;
+    for (int i = 0; i < V; i++){ // Iterate through all vertices because the graph may be disconnected
+        if (!visited[i]){ // If the vertex is not visited, start DFS from it
+           ans = dfs(i,-1,adj,visited);
+           if(ans) return true;
+        }
+    }
+    return ans;
+}
 int main(){
     vector<vector<int>> edges = {
         {0, 1}, {1, 2}, {2, 0}, // Cycle exists
@@ -40,6 +69,6 @@ int main(){
     };
     int V = 6; // Number of vertices
     cout << "Does the undirected graph contain a cycle? ";
-    cout << (isCycle(V, edges) ? "Yes" : "No") << endl;
+    cout << (isCycleUsingDFS(V, edges) ? "Yes" : "No") << endl;
     return 0;
 }
