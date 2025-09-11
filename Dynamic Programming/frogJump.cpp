dacbf7696a69aaf9d218e496ebc5768946f17858
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-09-11 12:43:01 +0530

    Did Space Optimization

--- Dynamic Programming/frogJump.cpp
+++ Dynamic Programming/frogJump.cpp
@@ -32,19 +32,36 @@ int frogJumpMemoization(int n, vector<int> &heights, vector<int> &dp)
 
 int frogJumpTabulation(int n, vector<int> &height)
 {
-    int dp[n] = {0};
-    int first = INT_MAX;
-    int second = INT_MAX;
-    for (int i = 1; i < n; i++)
-    {
-        first = dp[i - 1] + abs(height[i] - height[i - 1]);
-        if (i > 1)
-        {
-            second = dp[i - 2] + abs(height[i] - height[i - 2]);
+    // int dp[n] = {0};
+    // int first = INT_MAX;
+    // int second = INT_MAX;
+    // for (int i = 1; i < n; i++)
+    // {
+    //     first = dp[i - 1] + abs(height[i] - height[i - 1]);
+    //     if (i > 1)
+    //     {
+    //         second = dp[i - 2] + abs(height[i] - height[i - 2]);
+    //     }
+    //     dp[i] = min(first, second);
+    // }
+    // return dp[n - 1];
+
+
+    // Space Optimization
+    int n = height.size();
+        int first = INT_MAX;
+        int second = INT_MAX;
+        int prev2 = 0, prev = 0,curr =0;
+        for(int i=1;i<n;i++){
+            first = prev + abs(height[i]-height[i-1]);
+            if(i>1){
+                second = prev2 + abs(height[i]-height[i-2]);
+            }
+            curr=min(first,second);
+            prev2=prev;
+            prev=curr;
         }
-        dp[i] = min(first, second);
-    }
-    return dp[n - 1];
+        return prev;
 }
 
 int main()
