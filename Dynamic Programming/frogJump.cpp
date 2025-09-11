84cf22fb8b7fff9c81cdb5295fb213824b49aed7
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-09-11 12:22:58 +0530

    frog jump dp03

---
+++ Dynamic Programming/frogJump.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+int frogJumpRecursion(int n, vector<int> &heights)
+{
+    if (n == 0)
+        return 0; // Base case: cost to reach the first stone is 0
+    int jumpOne = frogJumpRecursion(n - 1, heights) + abs(heights[n] - heights[n - 1]);
+    int jumpTwo = INT_MAX;
+    if (n > 1)
+    {
+        jumpTwo = frogJumpRecursion(n - 2, heights) + abs(heights[n] - heights[n - 2]);
+    }
+    return min(jumpOne, jumpTwo);
+}
+
+int frogJumpMemoization(int n, vector<int> &heights, vector<int> &dp)
+{
+    if (n == 0)
+        return 0; // Base case: cost to reach the first stone is 0
+    if (dp[n] != -1)
+    {
+        return dp[n]; // Return cached result if exists
+    }
+    int jumpOne = frogJumpMemoization(n - 1, heights, dp) + abs(heights[n] - heights[n - 1]);
+    int jumpTwo = INT_MAX;
+    if (n > 1)
+    {
+        jumpTwo = frogJumpMemoization(n - 2, heights, dp) + abs(heights[n] - heights[n - 2]);
+    }
+    return dp[n] = min(jumpOne, jumpTwo);
+}
+
+int frogJumpTabulation(int n, vector<int> &height)
+{
+    int dp[n] = {0};
+    int first = INT_MAX;
+    int second = INT_MAX;
+    for (int i = 1; i < n; i++)
+    {
+        first = dp[i - 1] + abs(height[i] - height[i - 1]);
+        if (i > 1)
+        {
+            second = dp[i - 2] + abs(height[i] - height[i - 2]);
+        }
+        dp[i] = min(first, second);
+    }
+    return dp[n - 1];
+}
+
+int main()
+{
+    vector<int> heights = {10, 20, 30, 10, 30, 50, 70, 20};
+    int n = heights.size();
+    vector<int> dp(n, -1);
+    cout << "Minimum cost (Tabulation): " << frogJumpTabulation(n, heights) << endl;
+    return 0;
+}
--- Dynamic Programming/notes.md
+++ Dynamic Programming/notes.md
@@ -9,7 +9,15 @@ Conditions are:
 * Hrr level prr multiple recursive calls ho rahi hongi (must have condition) jo ki overlapping hongi
 * Optimal/Maximum/Minimum puch rhe honge
 
+#### DP methods
+* Memomization (Top-Down)
+* Tabulation (Bottom-up)
+
 ### Steps:
 * First try to write recursive solution
 * then try to memoize
 * then use tabulation
+
+### What to memoize ? 
+Look at the parameter which is changing and make a table for that parameter
+
